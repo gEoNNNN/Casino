61ecf0b72211bdfaa3f88f5547003d17a299e778
unknown <euge.ostafi@isa.utm.md>
2025-05-03 08:49:04 +0300

    project initialization

---
+++ Casino/app/welcome/welcome.tsx
@@ -0,0 +1,10 @@
+import logoDark from "./logo-dark.svg";
+import logoLight from "./logo-light.svg";
+
+export function Welcome() {
+  return (
+    <main className="min-h-screen w-full bg-[#222B4C] text-white flex items-center justify-center">
+      <h1 className="text-2xl font-bold">75.38 mdl</h1>
+    </main>
+  );
+}
