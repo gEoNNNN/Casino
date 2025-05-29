3a3f1f5a201259c6fa41945820e9ae69d5172feb
unknown <euge.ostafi@isa.utm.md>
2025-05-29 12:11:12 +0300

    :bug: deploy bug fix

--- Casino/vite.config.ts
+++ Casino/vite.config.ts
@@ -4,5 +4,10 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  base: "/", // 👈 Important for correct routing on Netlify
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  build: {
+    outDir: "build/client", // 👈 Ensure Vite outputs to the correct folder
+    emptyOutDir: true,
+  },
 });
